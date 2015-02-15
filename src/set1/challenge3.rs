9ac29bc9a27ee25daea3d9565e9cc0bc9c419926
David King <dave@davbo.org>
2015-02-15 23:24:10 +0000

    Added challenge 4

--- src/set1/challenge3.rs
+++ src/set1/challenge3.rs
@@ -4,17 +4,19 @@ extern crate collections;
 use std::collections::BTreeMap;
 use std::collections::btree_map::Entry;
 use std::old_io::File;
+use std::env::current_dir;
+
 
 use self::collections::string::FromUtf8Error;
 use self::rustc_serialize::hex::FromHex;
+use self::rustc_serialize::hex::ToHex;
 
 use set1::challenge2::fixed_xor;
 
-static ENCRYPTED_MESSAGE : &'static str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 
 
-pub fn decrypt_message(test_key: &str) -> Result<String, FromUtf8Error> {
-    String::from_utf8(fixed_xor(ENCRYPTED_MESSAGE, test_key).from_hex().unwrap())
+pub fn decrypt_message(encrypted_message: &str, test_key: &str) -> Result<String, FromUtf8Error> {
+    String::from_utf8(fixed_xor(encrypted_message, test_key).from_hex().unwrap())
 }
 
 pub fn letter_frequency_from_file(file_path: &Path) -> BTreeMap<char, usize> {
@@ -37,20 +39,15 @@ pub fn letter_frequency_from_file(file_path: &Path) -> BTreeMap<char, usize> {
     count
 }
 
-
-#[test]
-fn challenge3() {
-    use std::env::current_dir;
-    use self::rustc_serialize::hex::ToHex;
-
-    let english_corpus = current_dir().unwrap().join("example.txt");
+pub fn single_character_xor(encrypted_message: &str) -> Vec<(usize, String)> {
+    let english_corpus = current_dir().unwrap().join("data").join("example.txt");
     let letter_count = letter_frequency_from_file(&english_corpus);
     let mut results: Vec<(usize, String)> = Vec::new();
     for x in 1u8.. 255 {
         let mut vec : Vec<u8> = vec![x];
-        vec.resize(ENCRYPTED_MESSAGE.len()/2, x);
+        vec.resize(encrypted_message.len()/2, x);
         let test_val = vec.to_hex();
-        let res = decrypt_message(test_val.as_slice());
+        let res = decrypt_message(encrypted_message, test_val.as_slice());
         match res {
             Ok(decrypted_message) => {
                 let mut score = 0;
@@ -68,6 +65,14 @@ fn challenge3() {
         }
     }
     results.sort_by(|&(s1, _), &(s2, _)| s1.cmp(&s2));
+    results
+}
+
+
+#[test]
+fn challenge3() {
+    let encrypted_message = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let mut results = single_character_xor(encrypted_message);
     let (_, ref msg) = results.pop().unwrap();
     assert_eq!("Cooking MC's like a pound of bacon", msg.as_slice());
 }
---
+++ src/set1/challenge4.rs
@@ -0,0 +1,22 @@
+#[test]
+fn challenge4() {
+    use std::old_io::File;
+    use std::old_io::BufferedReader;
+    use std::env::current_dir;
+    use set1::challenge3::single_character_xor;
+    let mut file = BufferedReader::new(File::open(&current_dir().unwrap().join("data").join("4.txt")));
+    for encrypted_message in file.lines() {
+        match encrypted_message {
+            Ok(candidate) => {
+                let mut results = single_character_xor(candidate.as_slice());
+                if results.len() > 0 {
+                    let (best_score, ref msg) = results.pop().unwrap();
+                    if best_score > 2000 {
+                        assert_eq!("Now that the party is jumping\n", msg.as_slice());
+                    }
+                }
+            },
+            Err(_) => {}
+        }
+    }
+}
